4cd4282fddc46d3494e0448b41ef52d6fe2d93fb
Paulo Aboim Pinto <aboimpinto@gmail.com>
2020-09-26 11:01:20 +0200

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace myAvaloniaApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private int countClick = 0;
+        private string greeting;
+
+        public string Greeting 
+        { 
+            get => this.greeting;
+            set => this.RaiseAndSetIfChanged(ref this.greeting, value);
+        }
+
+        public ICommand ClickMeCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            this.Greeting = "No clicks observed";
+
+            this.ClickMeCommand = ReactiveCommand.Create(this.OnClickMe); 
+        }
+
+        private void OnClickMe()
+        {
+            countClick ++;
+            this.Greeting = $"Clicks: {this.countClick}";
+        }
+    }
+}
